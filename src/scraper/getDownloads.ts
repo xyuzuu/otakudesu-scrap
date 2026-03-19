7f2193de5a91c0126b1f8093db055c12e44cd385
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-19 18:38:38 +0700

    refactor getDownloads function and update resolution format
    refactor getDownloads function to improve readability and add 'Mp4' prefix to resolution.

--- src/scraper/getDownloads.ts
+++ src/scraper/getDownloads.ts
@@ -1,29 +1,44 @@
-import {load} from 'cheerio';
+import { load } from 'cheerio';
 import * as phin from 'phin';
-import {Download} from '../types';
-import {OtakUtil} from '../util';
+import { Download } from '../types';
+import { OtakUtil } from '../util';
 
-export const getDownloads = async (requestUrl: string, url: string):
-Promise<Download[]> => {
+export const getDownloads = async (
+  requestUrl: string,
+  url: string
+): Promise<Download[]> => {
   if (!OtakUtil.validateDownloadUrl(url)) return [];
 
   const response = await phin({
     url: new URL(url, requestUrl),
   });
+
   const $ = load(response.body.toString('utf8'));
 
   if (!$('title').text().trim().match(/otaku/gi)) return [];
 
   const downloads = $(
-      url.match(/batch/gi) ? '.batchlink > ul > li' :
-        '#venkonten > .venser > .venutama > .download > ul > li',
-  ).map((_, el) => ({
-    resolution: $(el).find('strong').text().trim(),
-    links: $(el).find('a').map((_, el2) => ({
-      name: $(el2).text().trim(),
-      url: $(el2).attr('href') as string,
-    })).toArray(),
-  })).toArray<Download>();
+    url.match(/batch/gi)
+      ? '.batchlink > ul > li'
+      : '#venkonten > .venser > .venutama > .download > ul > li'
+  )
+    .map((_, el) => {
+      const resolution = $(el).find('strong').text().trim();
+
+      const links = $(el)
+        .find('a')
+        .map((_, el2) => ({
+          name: $(el2).text().trim(),
+          url: $(el2).attr('href') as string,
+        }))
+        .toArray();
+
+      return {
+        resolution: `Mp4 ${resolution}`,
+        links,
+      };
+    })
+    .toArray<Download>();
 
   if (url.match(/lengkap/gi) && new Set(downloads).size !== downloads.length) {
     $('#venkonten > .download > h4').each((index, element) => {
@@ -32,4 +47,4 @@ Promise<Download[]> => {
   }
 
   return downloads;
-};
+}
