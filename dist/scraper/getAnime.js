d4048e71332bdb092617cdac149516a1fdf09fdd
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 12:55:16 +0700

    refactor getAnime function for improved readability

--- dist/scraper/getAnime.js
+++ dist/scraper/getAnime.js
@@ -14,34 +14,31 @@ const cheerio_1 = require("cheerio");
 const phin = require("phin");
 const _1 = require(".");
 const constants_1 = require("../constants");
-const getAnime = (requestUrl, q, ext = false) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield phin({
-        url: new URL((0, constants_1.getSearchAnimeEndpoint)(q), requestUrl),
-    });
-    const $ = (0, cheerio_1.load)(response.body.toString());
-    return $('ul.chivsrc > li').map((_, element) => {
-        var _a;
-        return ({
-            name: $(element).find('h2').text().trim(),
-            image: $(element).find('img').attr('src'),
-            meta: $(element).find('.set').map((_, metaElement) => {
-                const text = $(metaElement).text().trim();
-                return {
-                    key: text.split(':')[0].trim().toLowerCase(),
-                    value: text.split(':')[1].replace(/\s/, '').toLowerCase(),
-                };
-            }).toArray()
-                .reduce((x, y) => {
-                x[y.key] = y.value.split(',').length > 1 ? y.value.split(',') : y.value;
-                return x;
-            }, {}),
-            url: $(element).find('h2 > a').attr('href'),
-            slug: (_a = $(element).find('h2 > a').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop(),
-            extra: () => {
-                var _a;
-                return (0, _1.getExtraAnime)(requestUrl, (_a = $(element).find('h2 > a').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop());
-            },
-        });
-    }).toArray();
+const getAnime = (requestUrl, q, ext = false) => __awaiter(void 0, void 0, void 0, function*() {
+   const response = yield phin({ url: new URL((0, constants_1.getSearchAnimeEndpoint)(q), requestUrl) });
+   const $ = (0, cheerio_1.load)(response.body.toString());
+   return $('ul.chivsrc > li').map((_, element) => {
+      var _a;
+      return ({
+         name: $(element).find('h2').text().trim(),
+         image: $(element).find('img').attr('src'),
+         meta: $(element).find('.set').map((_, metaElement) => {
+            const text = $(metaElement).text().trim();
+            return {
+               key: text.split(':')[0].trim().toLowerCase(),
+               value: text.split(':')[1].replace(/\s/, '').toLowerCase(),
+            };
+         }).toArray().reduce((x, y) => {
+            x[y.key] = y.value.split(',').length > 1 ? y.value.split(',') : y.value;
+            return x;
+         }, {}),
+         url: $(element).find('h2 > a').attr('href'),
+         slug: (_a = $(element).find('h2 > a').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop(),
+         extra: () => {
+            var _a;
+            return (0, _1.getExtraAnime)(requestUrl, (_a = $(element).find('h2 > a').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop());
+         },
+      });
+   }).toArray();
 });
 exports.getAnime = getAnime;
