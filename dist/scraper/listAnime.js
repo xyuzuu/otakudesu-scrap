bfc60fdf4614ebafa9c576740a049415fd18d2a7
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 13:00:19 +0700

    refactor getListAnime function for readability

--- dist/scraper/listAnime.js
+++ dist/scraper/listAnime.js
@@ -13,19 +13,17 @@ exports.getListAnime = void 0;
 const cheerio_1 = require("cheerio");
 const phin = require("phin");
 const constants_1 = require("../constants");
-const getListAnime = (requestUrl) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield phin({
-        url: new URL(constants_1.ListEndpoint.animeList, requestUrl),
-    });
-    const $ = (0, cheerio_1.load)(response.body.toString('utf8'));
-    return $('#abtext > .bariskelom').map((_, el) => {
-        var _a;
-        return ({
-            _index: parseInt($(el).find('.barispenz').text().trim()),
-            name: $(el).find('.hodebgst').attr('title'),
-            url: $(el).find('.hodebgst').attr('href'),
-            slug: (_a = $(el).find('.hodebgst').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop(),
-        });
-    }).toArray();
+const getListAnime = (requestUrl) => __awaiter(void 0, void 0, void 0, function*() {
+   const response = yield phin({ url: new URL(constants_1.ListEndpoint.animeList, requestUrl) });
+   const $ = (0, cheerio_1.load)(response.body.toString('utf8'));
+   return $('#abtext > .bariskelom').map((_, el) => {
+      var _a;
+      return ({
+         _index: parseInt($(el).find('.barispenz').text().trim()),
+         name: $(el).find('.hodebgst').attr('title'),
+         url: $(el).find('.hodebgst').attr('href'),
+         slug: (_a = $(el).find('.hodebgst').attr('href')) === null || _a === void 0 ? void 0 : _a.split('/').filter((s) => s.length).pop(),
+      });
+   }).toArray();
 });
 exports.getListAnime = getListAnime;
