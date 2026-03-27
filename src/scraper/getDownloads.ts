b26125f21144298a3903408fd30af587a2a72665
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 12:01:42 +0700

    refactor getDownloads function for clarity and efficiency

--- src/scraper/getDownloads.ts
+++ src/scraper/getDownloads.ts
@@ -4,47 +4,32 @@ import { Download } from "../types"
 import { OtakUtil } from "../util"
 
 export const getDownloads = async (
-  requestUrl: string,
-  url: string
-): Promise<Download[]> => {
+   requestUrl: string,
+   url: string
+): Promise < Download[] > => {
+   if (!OtakUtil.validateDownloadUrl(url)) return []
+   const { data } = await axios.get(new URL(url, requestUrl).toString())
+   const $ = load(data)
+   if (!$("title").text().trim().match(/otaku/gi)) return []
 
-  if (!OtakUtil.validateDownloadUrl(url)) return []
-
-  const { data } = await axios.get(new URL(url, requestUrl).toString())
-  const $ = load(data)
-
-  if (!$("title").text().trim().match(/otaku/gi)) return []
-
-  const downloads: Download[] = $(
-    url.match(/batch/gi)
-      ? ".batchlink > ul > li"
-      : "#venkonten > .venser > .venutama > .download > ul > li"
-  )
-    .map((_, el) => {
+   const downloads: Download[] = $(url.match(/batch/gi) ? ".batchlink > ul > li" : "#venkonten > .venser > .venutama > .download > ul > li").map((_, el) => {
       const resolution = $(el).find("strong").text().trim()
-
-      const links = $(el)
-        .find("a")
-        .map((_, el2) => ({
-          name: $(el2).text().trim(),
-          url: $(el2).attr("href") as string,
-        }))
-        .toArray()
-
+      const links = $(el).find("a").map((_, el2) => ({
+         name: $(el2).text().trim(),
+         url: $(el2).attr("href") as string,
+      })).toArray()
       return {
-        resolution: `Mp4 ${resolution}`,
-        links,
+         resolution: `${resolution}`,
+         links,
       }
-    })
-    .toArray()
+   }).toArray()
 
-  if (url.match(/lengkap/gi) && new Set(downloads.map(d => d.resolution)).size !== downloads.length) {
-    $("#venkonten > .download > h4").each((index, element) => {
-      if (downloads[index]) {
-        downloads[index]["title"] = $(element).text().trim()
-      }
-    })
-  }
-
-  return downloads
+   if (url.match(/lengkap/gi) && new Set(downloads.map(d => d.resolution)).size !== downloads.length) {
+      $("#venkonten > .download > h4").each((index, element) => {
+         if (downloads[index]) {
+            downloads[index]["title"] = $(element).text().trim()
+         }
+      })
+   }
+   return downloads
 }
