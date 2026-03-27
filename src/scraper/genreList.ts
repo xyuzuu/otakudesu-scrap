714d42af7f6ae5cc6a01b3282ee4295526511ec5
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 11:53:51 +0700

    refactor genreList.ts for consistent formatting

--- src/scraper/genreList.ts
+++ src/scraper/genreList.ts
@@ -1,22 +1,18 @@
-import {load} from 'cheerio';
+import { load } from 'cheerio';
 import * as phin from 'phin';
 
-import {ListEndpoint} from '../constants';
-import type {Genre} from '../types';
+import { ListEndpoint } from '../constants';
+import type { Genre } from '../types';
 
 export const getGenreList = async (requestUrl: string):
-Promise<Genre[]> => {
-  const response = await phin({
-    'url': new URL(ListEndpoint.genreList, requestUrl),
-  });
-
-  const $ = load(response.body.toString('utf8'));
-
-  return $('.venser > ul.genres > li > a')
-      .map((_, genEl) =>
-        ({name: $(genEl).text().trim(),
-          url: new URL($(genEl).attr('href')!,
-              requestUrl,
-          ).href,
-        })).toArray<Genre>();
-};
+   Promise < Genre[] > => {
+      const response = await phin({
+         'url': new URL(ListEndpoint.genreList, requestUrl),
+      });
+      const $ = load(response.body.toString('utf8'));
+  
+      return $('.venser > ul.genres > li > a').map((_, genEl) => ({
+         name: $(genEl).text().trim(),
+         url: new URL($(genEl).attr('href') !, requestUrl).href,
+      })).toArray < Genre > ();
+   };
