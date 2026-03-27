db6666a58781558d92f86c0bee50e1488e3b9447
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 12:56:36 +0700

    refactor getDownloads function for clarity

--- dist/scraper/getDownloads.js
+++ dist/scraper/getDownloads.js
@@ -11,29 +11,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDownloads = void 0;
-
 const axios_1 = require("axios");
 const cheerio_1 = require("cheerio");
-
-const getDownloads = (url) => __awaiter(void 0, void 0, void 0, function* () {
-    const { data } = yield (0, axios_1.default).get(url);
-    const $ = (0, cheerio_1.load)(data);
-    const urls = {};
-    $("div.download > ul > li").each((i, e) => {
-        const resolution = $(e).find("strong").text().trim();
-        $(e).find("a").each((_i, _e) => {
-            urls[resolution] = urls[resolution] ? urls[resolution] : [];
-            urls[resolution].push({
-                name: $(_e).text().trim(),
-                url: $(_e).attr("href"),
-            });
-        });
-    });
-    const result = Object.keys(urls).map(res => ({
-        resolution: `Mp4 ${res}`,
-        links: urls[res]
-    }));
-    return result;
+const getDownloads = (url) => __awaiter(void 0, void 0, void 0, function*() {
+   const { data } = yield(0, axios_1.default).get(url);
+   const $ = (0, cheerio_1.load)(data);
+   const urls = {};
+   $("div.download > ul > li").each((i, e) => {
+      const resolution = $(e).find("strong").text().trim();
+      $(e).find("a").each((_i, _e) => {
+         urls[resolution] = urls[resolution] ? urls[resolution] : [];
+         urls[resolution].push({
+            name: $(_e).text().trim(),
+            url: $(_e).attr("href"),
+         });
+      });
+   });
+   const result = Object.keys(urls).map(res => ({
+      resolution: `${res}`,
+      links: urls[res]
+   }));
+   return result;
 });
-
 exports.getDownloads = getDownloads;
