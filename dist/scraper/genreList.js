190736cad4f975c5e5bcc13d9cd23973cdde393e
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 12:53:40 +0700

    refactor getGenreList function for readability

--- dist/scraper/genreList.js
+++ dist/scraper/genreList.js
@@ -13,14 +13,12 @@ exports.getGenreList = void 0;
 const cheerio_1 = require("cheerio");
 const phin = require("phin");
 const constants_1 = require("../constants");
-const getGenreList = (requestUrl) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield phin({
-        'url': new URL(constants_1.ListEndpoint.genreList, requestUrl),
-    });
-    const $ = (0, cheerio_1.load)(response.body.toString('utf8'));
-    return $('.venser > ul.genres > li > a')
-        .map((_, genEl) => ({ name: $(genEl).text().trim(),
-        url: new URL($(genEl).attr('href'), requestUrl).href,
-    })).toArray();
+const getGenreList = (requestUrl) => __awaiter(void 0, void 0, void 0, function*() {
+   const response = yield phin({ 'url': new URL(constants_1.ListEndpoint.genreList, requestUrl) });
+   const $ = (0, cheerio_1.load)(response.body.toString('utf8'));
+   return $('.venser > ul.genres > li > a').map((_, genEl) => ({
+       name: $(genEl).text().trim(),
+       url: new URL($(genEl).attr('href'), requestUrl).href
+   })).toArray();
 });
 exports.getGenreList = getGenreList;
