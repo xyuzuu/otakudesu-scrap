564d49c569f099496b692404f1029b84fd93cdb5
Dimas Triyatno <125540833+xyuzuu@users.noreply.github.com>
2026-03-27 11:50:02 +0700

    refactor type definitions for consistency

--- src/types.ts
+++ src/types.ts
@@ -1,55 +1,56 @@
 /**
  * Typings for otakudesu
  */
-
-import {ResolvedReleaseDate} from './util';
+import { ResolvedReleaseDate } from './util';
 
 export interface Genre {
-    name: string;
-    url: string;
+   name: string;
+   url: string;
 };
 
 export interface Anime {
-    name: string;
-    image: string;
-    meta: Record<string, string | string[]>;
-    url: string;
-    /** used for getting anime information and download(s) url */
-    slug: string;
-    extra: () => Promise<ExtraAnime>;
+   name: string;
+   image: string;
+   meta: Record < string,
+   string | string[] > ;
+   url: string;
+   /** used for getting anime information and download(s) url */
+   slug: string;
+   extra: () => Promise < ExtraAnime > ;
 };
 
-export type AnimeListItem = Omit<Anime,
-'meta' | 'image'> & {
-    _index: number;
-};
+export type AnimeListItem = Omit < Anime,
+   'meta' | 'image' > & {
+      _index: number;
+   };
 
 export interface Episode {
-    title: string;
-    url: string;
-    date: string;
+   title: string;
+   url: string;
+   date: string;
 };
 
 export interface DownloadLink {
-    name: string;
-    url: string;
+   name: string;
+   url: string;
 };
 
 export interface Download {
-    resolution: string;
-    links: DownloadLink[];
-    title?: string;
+   resolution: string;
+   links: DownloadLink[];
+   title ? : string;
 }
 
-export type OngoingAnime = Omit<Anime, 'meta' | 'extra'> & {
-    releaseAt: ResolvedReleaseDate;
-    episode: number;
+export type OngoingAnime = Omit < Anime, 'meta' | 'extra' > & {
+   releaseAt: ResolvedReleaseDate;
+   episode: number;
 };
 
-export type ExtraAnime = Omit<Anime, 'meta' | 'slug' | 'extra'> & {
-    synopsis: string;
-    details: Record<string, string | string[]>;
-    episodes: Episode[];
+export type ExtraAnime = Omit < Anime, 'meta' | 'slug' | 'extra' > & {
+   synopsis: string;
+   details: Record < string,
+   string | string[] > ;
+   episodes: Episode[];
 };
 
 export type HomeAnimeUpdate = OngoingAnime;
